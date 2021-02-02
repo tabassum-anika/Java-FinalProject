3dba93d637e7fabc91bbba3070ef370da7335c97
Anika Tabassum <atabassum161150@bscse.uiu.ac.bd>
2021-02-02 21:14:06 +0600

    updated

---
+++ src/MyJava.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class MyJava {
+    public static void main(String[] args){
+     Front obj = new Front();
+     obj.setVisible(true);
+     
+     
+       Login obj1 = new Login();
+     obj.setVisible(true);
+    
+     
+       Home obj2 = new Home();
+     obj.setVisible(true);
+   
+     
+       Faculty obj3 = new Faculty();
+     obj.setVisible(true);
+    
+     
+     
+     
+       Admission obj4 = new Admission();
+     obj.setVisible(true);
+     
+       Department obj5 = new Department();
+     obj.setVisible(true);
+    
+       Results obj6 = new Results();
+     obj.setVisible(true);
+    
+       Calender obj7 = new Calender();
+     obj.setVisible(true);
+
+    Contacts obj8 = new Contacts();
+     obj.setVisible(true);
+
+   
+   
+      ClubNdForum obj9 = new ClubNdForum();
+     obj.setVisible(true);
+     
+     Academic obj10 = new Academic();
+     obj.setVisible(true); 
+     
+    }
+}
